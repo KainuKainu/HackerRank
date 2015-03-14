af4e96a597ca2b8ad2c70f58c4d103037af25582
Kainu <kainu.edu@gmail.com>
2015-03-14 16:18:17 -0700

    Still missing cases 4, 5, 6 (unk)

--- KaprekarNum.cpp
+++ KaprekarNum.cpp
@@ -10,30 +10,39 @@ int main() {
   string in_num;
   bool kapFound = false;
   bool i_found;
-  unsigned long min, max, i, j, square;
-  unsigned long l1, l2;                 // first and second half
-  unsigned long divPow;
-  unsigned long numLen;
+  unsigned long long min, max, i, j, square;
+  unsigned long long l1, l2;                 // first and second half
+  unsigned long long divPow;
+  unsigned long long numLen;
 
   /* Get range values */
   getline(cin, in_num);
-  min = (unsigned long) stol(in_num);
+  min = (unsigned long long) stol(in_num);
   getline(cin, in_num);
-  max = (unsigned long) stol(in_num);
+  max = (unsigned long long) stol(in_num);
 
   /* Loop through all integers in range */
   for (i = min; i <= max; i++) {
     i_found = false;
     numLen = (to_string(i)).length();
     square = i*i;
-    divPow = (unsigned long) pow(10, numLen);
+    divPow = (unsigned long long) pow(10, numLen);
     l2 = square % divPow;
     l1 = (square - l2)/divPow;
-    if ( i == l1 + l2 )
+    if ( l2 && (i == l1 + l2) )
       i_found = true;
     else {
       for (j = 1; (j <= numLen) && (l1%10 == 0); j++) {
         l1 /= 10;
+        if ( i == l1 + l2 )
+          i_found = true;
+      }
+    }
+    if (!i_found) {
+      divPow /= 10;
+      l2 = square % divPow;
+      if (l2) {
+        l1 = (square - l2)/divPow;
         if ( i == l1 + l2 )
           i_found = true;
       }
