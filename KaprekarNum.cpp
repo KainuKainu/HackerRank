a7a492bc1abdffce7ad5116fdb888fda9dcddea5
Kainu <kainu.edu@gmail.com>
2015-03-14 15:19:04 -0700

    Starting 2nd challenge

---
+++ KaprekarNum.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+  string p, q;
+  unsigned long min, max;
+
+  /* Get range values */
+  getline(cin, p);
+
+
+  return 0;
+}
