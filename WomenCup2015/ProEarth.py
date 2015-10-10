19eed8f7e8ce2d47078fa855530445688d52c0e0
Kainu <kainu.edu@gmail.com>
2015-10-10 16:13:43 -0700

    ch2 timeout

--- WomenCup2015/ProEarth.py
+++ WomenCup2015/ProEarth.py
@@ -1,14 +1,8 @@
 N,T = [int(num) for num in input().split()]
-k = int(N/6)
-m = N%6
-T -= k*4
-if m == 1 or m == 5:
-    T -= 1
-elif m == 2:
-    T -= 2
-elif m == 4:
-    T += 2
-if T < 0:
-    print(0)
-else:
-    print(T)
+x = -1
+for a in range (1,N+1):
+    T += x
+    if a%2 == 0:
+        if x < 0: x = -(x-1)
+        else: x = -(x+1)
+print(T)
