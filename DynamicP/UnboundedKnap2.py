59dd5b104590f1edcdc324d72d10c34a11359722
Kainu <kainu.edu@gmail.com>
2016-02-22 13:13:13 -0800

    minor change in UKnap2

--- DynamicP/UnboundedKnap2.py
+++ DynamicP/UnboundedKnap2.py
@@ -2,9 +2,9 @@ T = int(input())
 while T>0:
     N,K = [int(num) for num in input().split()]
     items = [0] + [int(num) for num in input().split()]
-    dp = [0]*(K+2)
+    dp = [0]*(K+1)
 
-    for w in range (1,K+2):
+    for w in range (1,K+1):
         tmp = 0
         for i in range (1,N+1):
             item = items[i]
